9a8a4bbf5d6630f70d6815a080d9145dca44714a
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-06-04 00:10:12 +0800

    Remove redundant wrapper class

--- lazy_ostream.h
+++ lazy_ostream.h
@@ -1,33 +1,12 @@
 #include <iostream>
-#include <ostream>
 
 class Voidifier {
 public:
-  template <typename T> void operator=(const T) {}
-};
-
-template <class CharT, class Traits> class LazyOstream {
-private:
-  using OstreamT = std::basic_ostream<CharT, Traits>;
-
-  OstreamT &ostream_;
-
-public:
-  LazyOstream(OstreamT &ostream) : ostream_(ostream) {}
-
-  LazyOstream &operator<<(OstreamT &(*f)(OstreamT &)) {
-    (*f)(this->ostream_);
-    return *this;
-  }
-
-  template <typename T> LazyOstream &operator<<(const T &t) {
-    this->ostream_ << t;
-    return *this;
-  }
+  template <typename T> void operator=(T &) {}
 };
 
 #define LAZY_OSTREAM(ostream, enabled)                                         \
-  (!enabled) ? (void)0 : Voidifier() = LazyOstream(ostream)
+  (!enabled) ? (void)0 : Voidifier() = ostream
 #define LAZY_COUT(enabled) LAZY_OSTREAM(std::cout, enabled)
 #define LAZY_CERR(enabled) LAZY_OSTREAM(std::cerr, enabled)
 #define LAZY_CLOG(enabled) LAZY_OSTREAM(std::clog, enabled)
