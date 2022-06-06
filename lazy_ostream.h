81df5f6537dd39c49cc372397f0dad435a9dd587
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-06-06 22:11:22 +0800

    Add namespace and header guard

--- lazy_ostream.h
+++ lazy_ostream.h
@@ -1,4 +1,9 @@
+#ifndef LAZY_OSTREAM_H_
+#define LAZY_OSTREAM_H_
+
 #include <iostream>
+
+namespace lazy_ostream {
 
 class Voidifier {
 public:
@@ -10,3 +15,7 @@ public:
 #define LAZY_COUT(enabled) LAZY_OSTREAM(std::cout, enabled)
 #define LAZY_CERR(enabled) LAZY_OSTREAM(std::cerr, enabled)
 #define LAZY_CLOG(enabled) LAZY_OSTREAM(std::clog, enabled)
+
+} // namespace lazy_ostream
+
+#endif // LAZY_OSTREAM_H_
