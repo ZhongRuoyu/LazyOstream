7409f9678b408f00679c2cc193ae3df6545de560
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-06-08 23:23:47 +0800

    Make voidifier shared across calls

--- lazy_ostream.h
+++ lazy_ostream.h
@@ -7,11 +7,13 @@ namespace lazy_ostream {
 
 class Voidifier {
 public:
-  template <typename T> void operator=(T &) {}
+  template <typename T> constexpr void operator=(T &) const {}
 };
 
+static constexpr Voidifier voidifier;
+
 #define LAZY_OSTREAM(ostream, enabled)                                         \
-  (!enabled) ? (void)0 : Voidifier() = ostream
+  (!enabled) ? (void)0 : lazy_ostream::voidifier = ostream
 #define LAZY_COUT(enabled) LAZY_OSTREAM(std::cout, enabled)
 #define LAZY_CERR(enabled) LAZY_OSTREAM(std::cerr, enabled)
 #define LAZY_CLOG(enabled) LAZY_OSTREAM(std::clog, enabled)
